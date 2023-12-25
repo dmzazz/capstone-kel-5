75bb0c44e5caa04ac5331f7130ab03dc22fe0eeb
Ivannaaqidatul <ivannaaqidatul5@gmail.com>
2023-12-25 23:00:32 +0700

    ivanna merubah AppointmentForm.js, BookAppointment.js, AppointmentForm.css, menambahkan book-doctor-appointment-form.png

--- src/Components/AppointmentForm.js
+++ src/Components/AppointmentForm.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../Styles/AppointmentForm.css";
 import { ToastContainer, toast } from "react-toastify";
+import doctorForm from "../Assets/book-doctor-appointment-form.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronCircleLeft } from "@fortawesome/free-solid-svg-icons";
 
 function AppointmentForm() {
   useEffect(() => {
@@ -9,6 +12,7 @@ function AppointmentForm() {
   });
 
   const [patientName, setPatientName] = useState("");
+  const [patientEmail, setPatientEmail] = useState("");
   const [patientNumber, setPatientNumber] = useState("");
   const [patientGender, setPatientGender] = useState("default");
   const [appointmentTime, setAppointmentTime] = useState("");
@@ -25,6 +29,12 @@ function AppointmentForm() {
       errors.patientName = "Patient name is required";
     } else if (patientName.trim().length < 8) {
       errors.patientName = "Patient name must be at least 8 characters";
+    }
+
+    if (!patientEmail.trim()) {
+      errors.patientEmail = "Patient email is required";
+    } else if (!/\S+@\S+\.\S+/.test(patientEmail)) {
+      errors.patientEmail = "Patient email is invalid";
     }
 
     if (!patientNumber.trim()) {
@@ -56,6 +66,7 @@ function AppointmentForm() {
 
     // Reset form fields and errors after successful submission
     setPatientName("");
+    setPatientEmail("");
     setPatientNumber("");
     setPatientGender("default");
     setAppointmentTime("");
@@ -71,97 +82,156 @@ function AppointmentForm() {
 
   return (
     <div className="appointment-form-section">
-      <h1 className="legal-siteTitle">
-        <Link to="/">
-          Health <span className="legal-siteSign">+</span>
+      <div
+        className="d-flex position-relative flex-column justify-content-center gap-2"
+        style={{
+          height: "100vh",
+          maxHeight: "100vh",
+        }}
+      >
+        <Link
+          to="/"
+          className="fw-bold position-absolute"
+          title="Go to Home Page"
+          style={{
+            top: "20px",
+            left: "20px",
+          }}
+        >
+          <FontAwesomeIcon
+            icon={faChevronCircleLeft}
+            style={{
+              fontSize: "30px",
+              paddingRight: "6px",
+            }}
+          />
         </Link>
-      </h1>
-
-      <div className="form-container">
-        <h2 className="form-title">
-          <span>Book Appointment Online</span>
-        </h2>
-
-        <form className="form-content" onSubmit={handleSubmit}>
-          <label>
-            Patient Full Name:
-            <input
-              type="text"
-              value={patientName}
-              onChange={(e) => setPatientName(e.target.value)}
-              required
-            />
-            {formErrors.patientName && <p className="error-message">{formErrors.patientName}</p>}
-          </label>
-
-          <br />
-          <label>
-            Patient Phone Number:
-            <input
-              type="text"
-              value={patientNumber}
-              onChange={(e) => setPatientNumber(e.target.value)}
-              required
-            />
-            {formErrors.patientNumber && <p className="error-message">{formErrors.patientNumber}</p>}
-          </label>
-
-          <br />
-          <label>
-            Patient Gender:
-            <select
-              value={patientGender}
-              onChange={(e) => setPatientGender(e.target.value)}
-              required
-            >
-              <option value="default">Select</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="private">I will inform Doctor only</option>
-            </select>
-            {formErrors.patientGender && <p className="error-message">{formErrors.patientGender}</p>}
-          </label>
-
-          <br />
-          <label>
-            Preferred Appointment Time:
-            <input
-              type="datetime-local"
-              value={appointmentTime}
-              onChange={(e) => setAppointmentTime(e.target.value)}
-              required
-            />
-            {formErrors.appointmentTime && <p className="error-message">{formErrors.appointmentTime}</p>}
-          </label>
-
-          <br />
-          <label>
-            Preferred Mode:
-            <select
-              value={preferredMode}
-              onChange={(e) => setPreferredMode(e.target.value)}
-              required
-            >
-              <option value="default">Select</option>
-              <option value="voice">Voice Call</option>
-              <option value="video">Video Call</option>
-            </select>
-            {formErrors.preferredMode && <p className="error-message">{formErrors.preferredMode}</p>}
-          </label>
-
-          <br />
-          <button type="submit" className="text-appointment-btn">
-            Confirm Appointment
-          </button>
-
-          <p className="success-message" style={{display: isSubmitted ? "block" : "none"}}>Appointment details has been sent to the patients phone number via SMS.</p>
-        </form>
+        <h1 className="legal-siteTitle">
+          <Link to="/" className="fw-bold">
+            Self<span className="legal-siteSign">Care</span>
+          </Link>
+        </h1>
+        <div className="d-flex flex-column align-items-center">
+          <h2>Book Your Appointment </h2>
+          <img src={doctorForm} alt="doctor-form" className="doctor-form-img" />
+        </div>
+        <div
+          className="text-center position-absolute"
+          style={{
+            bottom: "20px",
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        >
+          <p>© 2023 SelfCare. All rights reserved.</p>
+        </div>
       </div>
+      <div className="d-flex flex-column w-100 h-100">
+        <div className="form-container px-5 h-100">
+          <h2 className="form-title">
+            <span>Appointment Form</span>
+          </h2>
 
-      <div className="legal-footer">
-        <p>© 2023 Health+. All rights reserved.</p>
+          <form className="form-content" onSubmit={handleSubmit}>
+            <label>
+              Patient Full Name:
+              <input
+                type="text"
+                value={patientName}
+                onChange={(e) => setPatientName(e.target.value)}
+                required
+              />
+              {formErrors.patientName && (
+                <p className="error-message">{formErrors.patientName}</p>
+              )}
+            </label>
+
+            <label>
+              Patient Email:
+              <input
+                type="email"
+                value={patientEmail}
+                onChange={(e) => setPatientEmail(e.target.value)}
+                required
+              />
+              {formErrors.patientEmail && (
+                <p className="error-message">{formErrors.patientEmail}</p>
+              )}
+            </label>
+
+            <label>
+              Patient Phone Number:
+              <input
+                type="text"
+                value={patientNumber}
+                onChange={(e) => setPatientNumber(e.target.value)}
+                required
+              />
+              {formErrors.patientNumber && (
+                <p className="error-message">{formErrors.patientNumber}</p>
+              )}
+            </label>
+
+            <label>
+              Patient Gender:
+              <select
+                value={patientGender}
+                onChange={(e) => setPatientGender(e.target.value)}
+                required
+              >
+                <option value="default">Select</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="private">I will inform Doctor only</option>
+              </select>
+              {formErrors.patientGender && (
+                <p className="error-message">{formErrors.patientGender}</p>
+              )}
+            </label>
+
+            <label>
+              Preferred Appointment Time:
+              <input
+                type="datetime-local"
+                value={appointmentTime}
+                onChange={(e) => setAppointmentTime(e.target.value)}
+                required
+              />
+              {formErrors.appointmentTime && (
+                <p className="error-message">{formErrors.appointmentTime}</p>
+              )}
+            </label>
+
+            <label>
+              Preferred Mode:
+              <select
+                value={preferredMode}
+                onChange={(e) => setPreferredMode(e.target.value)}
+                required
+              >
+                <option value="default">Select</option>
+                <option value="voice">Voice Call</option>
+                <option value="video">Video Call</option>
+              </select>
+              {formErrors.preferredMode && (
+                <p className="error-message">{formErrors.preferredMode}</p>
+              )}
+            </label>
+
+            <button type="submit" className="text-appointment-btn mt-4">
+              Confirm Appointment
+            </button>
+
+            <p
+              className="success-message"
+              style={{ display: isSubmitted ? "block" : "none" }}
+            >
+              Appointment details has been sent to the patients phone number via
+              SMS.
+            </p>
+          </form>
+        </div>
       </div>
-
       <ToastContainer autoClose={5000} limit={1} closeButton={false} />
     </div>
   );
--- src/Components/BookAppointment.js
+++ src/Components/BookAppointment.js
@@ -1,20 +1,10 @@
 import React from "react";
 import Doctor from "../Assets/doctor-book-appointment.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCircleCheck,
-  faCalendarCheck,
-} from "@fortawesome/free-solid-svg-icons";
-import { useNavigate  } from "react-router-dom";
+import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import "../Styles/BookAppointment.css";
 
 function BookAppointment() {
-  const navigate = useNavigate();
-
-  const handleBookAppointmentClick = () => {
-    navigate("/appointment");
-  };
-
   return (
     <div className="ba-section">
       <div className="ba-image-content">
@@ -23,32 +13,28 @@ function BookAppointment() {
 
       <div className="ba-text-content">
         <h3 className="ba-title">
-          <span>Why Choose Health</span>
+          <span>Kenapa memilih SelfCare?</span>
         </h3>
-        <p className="ba-description">          
-        Temukan alasan memilih Health Plus untuk kebutuhan kesehatan Anda. 
-        Rasakan perawatan ahli, kemudahan, dan solusi yang dipersonalisasi, menjadikan kesejahteraan Anda 
-        sebagai prioritas utama kami. Bergabunglah dengan kami dalam perjalanan menuju kesehatan yang lebih baik 
-        dan kehidupan yang lebih bahagia.
+        <p className="ba-description">
+          Temukan alasan memilih Health Plus untuk kebutuhan kesehatan Anda.
+          Rasakan perawatan ahli, kemudahan, dan solusi yang dipersonalisasi,
+          menjadikan kesejahteraan Anda sebagai prioritas utama kami.
+          Bergabunglah dengan kami dalam perjalanan menuju kesehatan yang lebih
+          baik dan kehidupan yang lebih bahagia.
         </p>
 
         <p className="ba-checks ba-check-first">
-          <FontAwesomeIcon icon={faCircleCheck} style={{ color: "#1E8FFD" }} /> Dokter professional terbaik
+          <FontAwesomeIcon icon={faCircleCheck} style={{ color: "#1E8FFD" }} />{" "}
+          Dokter professional terbaik
         </p>
         <p className="ba-checks">
-          <FontAwesomeIcon icon={faCircleCheck} style={{ color: "#1E8FFD" }} /> Sistem informasi jelas
+          <FontAwesomeIcon icon={faCircleCheck} style={{ color: "#1E8FFD" }} />{" "}
+          Sistem informasi jelas
         </p>
         <p className="ba-checks ba-check-last">
-          <FontAwesomeIcon icon={faCircleCheck} style={{ color: "#1E8FFD" }} />  Jadwal Mudah dan Cepat
+          <FontAwesomeIcon icon={faCircleCheck} style={{ color: "#1E8FFD" }} />{" "}
+          Jadwal Mudah dan Cepat
         </p>
-
-        <button
-          className="text-appointment-btn"
-          type="button"
-          onClick={handleBookAppointmentClick}
-        >
-          <FontAwesomeIcon icon={faCalendarCheck} /> Book Appointment
-        </button>
       </div>
     </div>
   );
